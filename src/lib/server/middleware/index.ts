97367983805ef9ff0b3cadabd49cc98bba801fda
ATQQ <2604395430@qq.com>
2021-04-09 21:34:54 +0800

    feat: 成功状态吗默认为0

--- src/lib/server/middleware/index.ts
+++ src/lib/server/middleware/index.ts
@@ -80,7 +80,7 @@ export function expandHttpRespPrototype(http: ServerOptions): void {
     }
 
     resp.success = function (data?) {
-        this.json(new Result(200, 'ok', data))
+        this.json(new Result(0, 'ok', data))
     }
 
     resp.fail = function (code, msg, data) {
