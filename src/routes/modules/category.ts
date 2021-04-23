1cd729297244025669d650799b64fbe4ed03254b
ATQQ <2604395430@qq.com>
2021-04-24 00:13:07 +0800

    feat: 完成分类接口的日志接入

--- src/routes/modules/category.ts
+++ src/routes/modules/category.ts
@@ -1,64 +1,109 @@
 import { CategoryError } from '@/constants/errorMsg'
 import { deleteCategory, insertCategory, selectCategory } from '@/db/categoryDb'
+import { addBehavior } from '@/db/logDb'
 import { updateTask } from '@/db/taskDb'
 import Router from '@/lib/Router'
 import { getUserInfo } from '@/utils/userUtil'
 
 const router = new Router('category')
 
+/**
+ * 新建分类
+ */
 router.post('create', async (req, res) => {
   const { name } = req.body
-  const { id: user_id } = await getUserInfo(req)
+  const { id: user_id, account: logAccount } = await getUserInfo(req)
   const categories = await selectCategory({
     user_id,
     name,
   })
   // 分类已存在
   if (categories.length !== 0) {
+    addBehavior(req, {
+      module: 'category',
+      msg: `创建分类失败(已存在) 用户:${logAccount} 名称:${name}`,
+      data: {
+        name,
+        account: logAccount,
+      },
+    })
     res.failWithError(CategoryError.exist)
     return
   }
-  insertCategory({
+  addBehavior(req, {
+    module: 'category',
+    msg: `创建分类成功 用户:${logAccount} 名称:${name}`,
+    data: {
+      name,
+      account: logAccount,
+    },
+  })
+  await insertCategory({
     user_id,
     name,
-  }).then(() => {
-    res.success()
   })
+  res.success()
 }, {
   needLogin: true,
 })
 
+/**
+ * 获取分类列表
+ */
 router.get('', async (req, res) => {
-  const { id: user_id } = await getUserInfo(req)
-  selectCategory({
+  const { id: user_id, account: logAccount } = await getUserInfo(req)
+  addBehavior(req, {
+    module: 'category',
+    msg: `获取分类列表 用户:${logAccount}`,
+    data: {
+      account: logAccount,
+    },
+  })
+  const categories = await selectCategory({
     user_id,
-  }).then((categories) => {
-    categories.forEach((v) => {
-      v.user_id = undefined
-    })
-    res.success({
-      categories,
-    })
+  })
+  categories.forEach((v) => {
+    v.user_id = undefined
+  })
+  res.success({
+    categories,
   })
 }, {
   needLogin: true,
 })
 
+/**
+ * 删除指定分类
+ */
 router.delete('/:key', async (req, res) => {
   const { key } = req.params
-  const { id: user_id } = await getUserInfo(req)
-  deleteCategory({
+  const { id: user_id, account: logAccount } = await getUserInfo(req)
+  const [c] = await selectCategory({
     k: key,
     user_id,
-  }).then(() => {
+  })
+  if (c) {
+    await deleteCategory({
+      id: c.id,
+    })
     // 删掉的分类下的所有任务变为默认分类
-    updateTask({
+    await updateTask({
       categoryKey: 'default',
     }, {
       categoryKey: key,
     })
-    res.success()
-  })
+
+    // 记录日志
+    addBehavior(req, {
+      module: 'category',
+      msg: `删除指定分类 用户:${logAccount} 名称:${c.name}`,
+      data: {
+        account: logAccount,
+        name: c.name,
+      },
+    })
+  }
+  res.success()
 }, {
   needLogin: true,
 })
