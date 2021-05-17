6bb80d17142c48494a14934b2c725f9e50310cd4
ATQQ <2604395430@qq.com>
2021-05-17 20:14:59 +0800

    chore: 修改Pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
         name: 'easypicker2-test',
         script: 'src/server.ts',
         interpreter: './node_modules/.bin/ts-node',
-        watch: ['src'],
+        watch: false,
+        ignore_watch:['upload','node_modules'],
         log_type: 'json',
         env: {
             NODE_ENV: 'development',
