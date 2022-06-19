6d2995cba825485cbd12cb27c806ff5afffef725
sugar <engineerzjl@foxmail.com>
2022-06-19 16:33:45 +0800

    feat: 添加查询和更新需求状态API

--- src/controllers/wish.ts
+++ src/controllers/wish.ts
@@ -3,12 +3,16 @@ import {
   Post,
   ReqBody,
   FWRequest,
+  Get,
+  Put,
 } from 'flash-wolves'
 import { Wish, WishStatus } from '@/db/model/wish'
-import { addWishData } from '@/db/wishDb'
-import { getUniqueKey } from '@/utils/stringUtil'
+import { addWishData, findWish, updateWish } from '@/db/wishDb'
+import { getObjectIdDate, getUniqueKey } from '@/utils/stringUtil'
 import { addBehavior } from '@/db/logDb'
+import { USER_POWER } from '@/db/model/user'
 
+const adminPower = { needLogin: true, userPower: USER_POWER.SUPER }
 @RouterController('wish')
 export default class WishController {
   /**
@@ -16,8 +20,8 @@ export default class WishController {
    */
   @Post('add', { CORS: true })
   async addWish(
-    @ReqBody() body:Wish,
-      req:FWRequest,
+    @ReqBody() body: Wish,
+      req: FWRequest,
   ) {
     addBehavior(req, {
       module: 'wish',
@@ -31,5 +35,24 @@ export default class WishController {
       status: WishStatus.REVIEW,
     }
     await addWishData(wish)
+  }
+
+  @Get('all', adminPower)
+  async getAllWish() {
+    const wishes = await findWish({})
+    return wishes.map((wish) => {
+      const {
+        title, des, status, id, contact,
+      } = wish
+      const createDare = getObjectIdDate(id)
+      return {
+        title, des, status, id, contact, createDare,
+      }
+    })
+  }
+
+  @Put('update', adminPower)
+  async updateWishStatus(@ReqBody('id') id: string, @ReqBody('status') status: WishStatus) {
+    await updateWish({ id }, { $set: { status } })
   }
 }
