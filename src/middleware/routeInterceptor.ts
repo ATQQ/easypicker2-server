92d02f43ae32e7b5bfd13194cb0e86fe3ed68bbc
sugar <engineerzjl@foxmail.com>
2022-06-22 11:07:29 +0800

    chore: 优化预检请求

--- src/middleware/routeInterceptor.ts
+++ src/middleware/routeInterceptor.ts
@@ -9,6 +9,13 @@ const interceptor: Middleware = async (req, res) => {
   if (!meta || Object.keys(meta).length === 0) return
   // console.log(`路由拦截:${req.method} - ${req.url}`)
   const { needLogin, userPower, CORS } = meta
+  if (CORS) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+  }
+
   if (needLogin && (!req.headers.token || !(await getUserInfo(req)))) {
     addBehavior(req, {
       module: 'interceptor',
@@ -33,13 +40,6 @@ const interceptor: Middleware = async (req, res) => {
       })
       res.failWithError(publicError.request.notLogin)
     }
-  }
-
-  if (CORS) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
   }
 }
 export default interceptor
--- src/middleware/serverInterceptor.ts
+++ src/middleware/serverInterceptor.ts
@@ -24,7 +24,7 @@ const interceptor: Middleware = async (req, res) => {
     res.statusCode = 204
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+    res.setHeader('Access-Control-Allow-Headers', '*')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     res.end()
     return
